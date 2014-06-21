a0fa8d6ea65370e822d9523c6a61015bee6e0e9b
Sergey Chikuyonok <serge.che@gmail.com>
2014-06-21 21:29:37 +0400

    Minor fixes and improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('watch', function() {
 	gulp.watch(['lib/**/*.{js,css}'], ['default']);
 });
 
 gulp.task('default', function() {
-	gulp.src(['./manifest.json', './lib/view.css'])
+	gulp.src('./ui/ui.css')
+		.pipe(minifyCss({processImport: true}))
+		.pipe(gulp.dest('out/'));
+
+	gulp.src(['./manifest.json'])
         .pipe(gulp.dest('./out'));
 
-	return gulp.src(['./lib/app.js', './lib/background.js'])
+	return gulp.src(['./lib/chrome-app.js', './lib/chrome-background.js'])
 		.pipe(browserify({
           insertGlobals : false,
           debug : false
