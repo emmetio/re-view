97187e44eaf5c35cb71de7ac883a623367d039c4
Sergey Chikuyonok <serge.che@gmail.com>
2014-06-29 17:51:22 +0400

    Better Chrome extension build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,43 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var minifyCss = require('gulp-minify-css');
+var del = require('del');
+var zip = require('gulp-zip');
+var merge = require('merge-stream');
+
+var output = {
+	chrome: './out/chrome'
+};
 
 gulp.task('watch', function() {
 	gulp.watch(['lib/**/*.{js,css}'], ['default']);
 });
 
-gulp.task('default', function() {
-	gulp.src('./ui/ui.css')
+gulp.task('chrome-compile', function() {
+	del.sync([output.chrome]);
+	
+	var css = gulp.src('./ui/ui.css')
 		.pipe(minifyCss({processImport: true}))
-		.pipe(gulp.dest('out/'));
+		.pipe(gulp.dest(output.chrome));
 
-	gulp.src(['./lib/chrome/manifest.json', './lib/chrome/*.png'])
-        .pipe(gulp.dest('./out'));
 
-	return gulp.src(['./lib/chrome/app.js', './lib/chrome/background.js'])
+	var js = gulp.src(['./lib/chrome/app.js', './lib/chrome/background.js'])
 		.pipe(browserify({
-          insertGlobals : false,
-          debug : false
-        }))
-        .pipe(gulp.dest('./out'));
+			insertGlobals : false,
+			debug : false
+		}))
+		.pipe(gulp.dest(output.chrome));
+
+	var misc = gulp.src(['./lib/chrome/manifest.json', './lib/chrome/*.png'])
+		.pipe(gulp.dest(output.chrome));
+
+	return merge(css, js, misc);
 });
+
+gulp.task('chrome', ['chrome-compile'], function() {
+	return gulp.src(['*.*'], {cwd: output.chrome})
+		.pipe(zip('chrome.zip'))
+		.pipe(gulp.dest(output.chrome));
+});
+
+gulp.task('default', ['chrome']);
